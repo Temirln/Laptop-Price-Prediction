16288a104d3dbba3217340fdf2c81d4e9ac71207
Temirln <t1mb3rmn@gmail.com>
2023-04-06 10:08:52 +0600

    Added only new laptops trained model

--- laptops/urls.py
+++ laptops/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from laptops.views import predict,index
+from laptops.views import predict,index,predict_only_new
 
 # app_name = 'predict'
 
 urlpatterns = [
     path('predict/',predict, name="predictlaptop"),
+    path('predict_new/',predict_only_new,name="predict_only_new"),
     path('',index, name="home")
 ]
--- laptops/views.py
+++ laptops/views.py
@@ -62,7 +62,7 @@ def predict(request):
         status = request.POST['status']
         brand = request.POST['brand'].upper()
 
-        excel_file = r'C:\Users\Lenovo\Jupeter\RMT\marketplaces_parsing\Endterm\df.xlsx'
+        excel_file = r'D:\IDE Projects\VS Code Projects\Laptop Price Prediction\rmt\files\df.xlsx'
         df = pd.read_excel(excel_file)
         
         label_encoder = preprocessing.LabelEncoder()
@@ -76,7 +76,7 @@ def predict(request):
         status_v = dict(zip(list(p_status),df['status'].to_list()))
 
         # model = pd.read_pickle(r"C:\Users\Lenovo\Jupeter\RMT\marketplaces_parsing\Endterm\random.pickle")
-        model = pd.read_pickle(r"C:\Users\Lenovo\Jupeter\RMT\marketplaces_parsing\Endterm\random_full_dataset.pickle")
+        model = pd.read_pickle(r"D:\IDE Projects\VS Code Projects\Laptop Price Prediction\rmt\files\random_full_dataset.pickle")
 
 
         result = model.predict([[brand_v[brand],display,ssd,ram,status_v[status]]])
@@ -84,3 +84,39 @@ def predict(request):
         context['predict'] = int(result[0])
 
     return render(request,"form1.html",context)
+
+def predict_only_new(request):
+
+    context={
+        'values': request.POST,
+    }
+
+    if request.method == "POST":
+        ssd = request.POST['ssd']
+        ram = request.POST['ram']
+        display = request.POST['display'] 
+        microprocessors = request.POST['microprocessors'].upper()
+        brand = request.POST['brand'].upper()
+
+        excel_file = r'D:\IDE Projects\VS Code Projects\Laptop Price Prediction\rmt\files\df_only_new.xlsx'
+        df = pd.read_excel(excel_file)
+        
+        label_encoder = preprocessing.LabelEncoder()
+        p_brand = df['brand']
+        p_microprocessors = df['microprocessors']
+
+        df['brand'] = label_encoder.fit_transform(df['brand'])
+        df['microprocessors'] = label_encoder.fit_transform(df['microprocessors'])
+
+        brand_v = dict(zip(list(p_brand),df['brand'].to_list()))
+        microprocessors_v = dict(zip(list(p_microprocessors),df['microprocessors'].to_list()))
+
+        # model = pd.read_pickle(r"C:\Users\Lenovo\Jupeter\RMT\marketplaces_parsing\Endterm\random.pickle")
+        model = pd.read_pickle(r"D:\IDE Projects\VS Code Projects\Laptop Price Prediction\rmt\files\random_only_new.pickle")
+
+
+        result = model.predict([[brand_v[brand],display,ssd,ram,microprocessors_v[microprocessors]]])
+
+        context['predict'] = int(result[0])
+
+    return render(request,"form1 only_new.html",context) 
