ec87f1d694c3601427ad1df43bb7c1b211870111
Temirln <t1mb3rmn@gmail.com>
2023-05-13 23:35:53 +0600

    verCel

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,7 @@
 echo "Build Start Temirlan"
 # build_files.sh
-<<<<<<< HEAD
 pip install -r requirements.txt
 # python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py runserver
-=======
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
->>>>>>> parent of e2bc563 (test)
 
 echo "Build End Temirlan"
