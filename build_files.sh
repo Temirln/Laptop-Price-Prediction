ec4a0e2fab83253217de9fd6a08e3809fe1399c5
Temirln <t1mb3rmn@gmail.com>
2023-05-14 01:33:32 +0600

    sqlite3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "Build Start Temirlan"
 # build_files.sh
+sudo apt-get install python-sqlite
 pip install -r requirements.txt
 # python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py runserver
