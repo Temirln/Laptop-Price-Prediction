2525ecff639b49bf8bef2615449ebb6114df6d08
Temirln <t1mb3rmn@gmail.com>
2023-05-13 18:44:11 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
+echo "Build Start Temirlan"
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build End Temirlan"
