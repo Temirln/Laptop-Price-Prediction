aece6ed862964803af64a0bd2f8d016baaa64834
Temirln <t1mb3rmn@gmail.com>
2023-05-14 09:44:16 +0600

    Update settings.py

--- rmt/settings.py
+++ rmt/settings.py
@@ -130,5 +130,5 @@ import os
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
