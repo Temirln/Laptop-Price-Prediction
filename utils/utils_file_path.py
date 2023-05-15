6b9a07466ff7609877612e55e5315253eccfe518
Temirln <t1mb3rmn@gmail.com>
2023-05-15 09:21:46 +0600

    Update utils_file_path.py

--- utils/utils_file_path.py
+++ utils/utils_file_path.py
@@ -3,6 +3,5 @@ import os
 from backend.settings import BASE_DIR
 
 def get_full_path(path):
-    dirname = os.path.dirname(__file__)
     filename = os.path.join(BASE_DIR, path)
     return filename
